5b39c9526c0787f737f2e4888cc84dbf848e0696
tmansuroglu <mansuroglu99@gmail.com>
2025-12-24 23:37:02 +0300

    refactor: removed unused files

--- lib/db/record/delete-record.ts
+++
@@ -1,8 +0,0 @@
-import "server-only";
-
-import { db } from "../db";
-import { records } from "../schema";
-import { eq } from "drizzle-orm";
-
-export const deleteRecord = async (recordId: string) =>
-  db.delete(records).where(eq(records.id, recordId)).returning();
