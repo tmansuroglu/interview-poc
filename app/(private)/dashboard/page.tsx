7ea231601bc643d779d71920837e384d0a31fcfa
tmansuroglu <mansuroglu99@gmail.com>
2025-12-24 23:34:29 +0300

    refactor: removed unnecessary suspense

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -1,14 +1,11 @@
 import ChartSection from "@/components/ChartSection";
 import RecordFormSection from "@/components/RecordFormSection";
-import { Suspense } from "react";
 
 export default function DashboardPage() {
   return (
     <main className="space-y-6">
       <h1>Dasboard</h1>
-      <Suspense fallback={"Loading chart.."}>
-        <ChartSection />
-      </Suspense>
+      <ChartSection />
       <RecordFormSection />
     </main>
   );
