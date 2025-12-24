7533639f904882d604d3338d9d6a71a92548ee44
tmansuroglu <mansuroglu99@gmail.com>
2025-12-24 23:26:53 +0300

    refactor: removed unnecessary code

--- actions/record/get-all-records-action.ts
+++ actions/record/get-all-records-action.ts
@@ -1,20 +1,10 @@
 import { getDrizzleErrorMessage } from "@/lib/db/get-drizzle-error-message";
 import { getAllRecords } from "@/lib/db/record/get-all-records";
-import { verifySession } from "@/lib/session/verify-session";
 import { GetAllRecordsState } from "@/lib/types";
 import { DrizzleQueryError } from "drizzle-orm";
-import { logoutUserAction } from "../auth/logout-user-action";
-import { redirect, RedirectType } from "next/navigation";
-import { PublicRoutes } from "@/lib/enums";
 
 export const getAllRecordsAction = async (): Promise<GetAllRecordsState> => {
   try {
-    const { firebaseUserId } = await verifySession();
-
-    if (!firebaseUserId) {
-      throw Error("logout");
-    }
-
     const allRecords = await getAllRecords();
 
     const yesCount = allRecords.filter((record) => record.vaccinated).length;
@@ -33,11 +23,6 @@ export const getAllRecordsAction = async (): Promise<GetAllRecordsState> => {
         success: false,
         message: getDrizzleErrorMessage(error),
       };
-    }
-
-    if (error instanceof Error && error.message === "logout") {
-      await logoutUserAction();
-      redirect(PublicRoutes.Login, RedirectType.replace);
     }
 
     console.log("Unexpected get all records error", error);
